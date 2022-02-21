22e28fbe46682cdddbef9969ffe37b8e8085d23f
Morgan Oneka <onekam@umich.edu>
2022-02-21 12:29:31 -0500

    Individual plots not work with no edge weight/color. Removed extraneous parameters to consensus plot

--- DifferentialNetworkVis.R
+++ DifferentialNetworkVis.R
@@ -198,7 +198,7 @@ split_by_group <- function(diff_network_object, group_label){
       EdgeFeatures = diff_network_object$EdgeFeatures[indices]
     )
     
-    sub_dno$NodeXY[,"Index"] = 1:length(indices)
+    # sub_dno$NodeXY[,"Index"] = 1:length(indices)
     
     return(sub_dno)
     
@@ -216,7 +216,7 @@ get_possible_values_individual <- function(diff_network_object, individual_featu
 }
 
 ############################ NETWORK VISUALIZATION ######################
-plot_individual <-function(diff_network_object,idx=1,edge_weight="Weight", edge_color="Type", colors=NULL){
+plot_individual <-function(diff_network_object,idx=1,edge_weight=NULL, edge_color=NULL, colors=NULL, nodetype="Node Type"){
   
   node1="N1"
   node2="N2"
@@ -228,15 +228,30 @@ plot_individual <-function(diff_network_object,idx=1,edge_weight="Weight", edge_
   edge_info <- merge(edge_info, node_xy_df, by.x=node1, by.y="Node", all.x=TRUE, all.y=FALSE)
   edge_info <- merge(edge_info, node_xy_df, by.x=node2, by.y="Node", all.x=TRUE, all.y=FALSE)
   
-  names(edge_info)[names(edge_info) == edge_weight] <- 'Weight'
-  names(edge_info)[names(edge_info) == edge_color] <- 'Type'
   
-  
+  if (!is.null(edge_weight)){
+    names(edge_info)[names(edge_info) == edge_weight] <- 'Weight'  
+  } else{
+    edge_info$Weight = 1
+  }
   edge_info$size <- 1.5
+  split_data = NULL
+  
+  
+  if (!is.null(edge_color)){
+    names(edge_info)[names(edge_info) == edge_color] <- 'Type'
+    split_data <- split(edge_info,diff_network_object$EdgeFeatures[[idx]][,edge_color])
+    
+  } else{
+    edge_info$Type = "Edge"
+    split_data <- list(edge_info)
+  }
+  
+  
+  
   
   
   # split based on edge_type
-  split_data <- split(edge_info,diff_network_object$EdgeFeatures[[idx]][,edge_color])
   g <- ggplot()
   
   if(is.null(colors)){
@@ -251,7 +266,7 @@ plot_individual <-function(diff_network_object,idx=1,edge_weight="Weight", edge_
     edges_loops <- splits[[1]]
     edges_nonloops <- splits[[2]]
     
-    weight_name = paste(names(split_data)[i], "Weight", sep="_")
+    weight_name = paste("Weight", names(split_data)[i], sep="_")
     
     edges_loops[,weight_name] <- edges_loops$Weight
     edges_nonloops[,weight_name] <- edges_nonloops$Weight
@@ -262,21 +277,31 @@ plot_individual <-function(diff_network_object,idx=1,edge_weight="Weight", edge_
       geom_curve(
         aes_string(x = "X.x", y = "Y.x", xend = "X.y", yend = "Y.y", color=weight_name, size="size"), alpha=0.8,  curvature = 50,angle = 270,
         data = edges_loops) + scale_color_gradient(low=colors[i,"low"],high=colors[i,"high"]) 
+    
+    if (is.null(edge_color)){
+      g = g + guides(color = FALSE)
+    } 
+    
+    if (is.null(edge_weight)){
+      g = g+ guides(size = FALSE)
+    } 
   }
   
   
   g <- g + geom_circle(aes(x0 = X, y0 = Y, r = radius, fill = Node), data=node_xy_df) + 
     geom_label_repel(aes(x=X,y=Y,label=Node),hjust=0, vjust=0, data=node_xy_df) +
-    guides(fill=guide_legend(title="Cell Type")) + guides(size = FALSE) +
+    guides(fill=guide_legend(title=nodetype)) + guides(size = FALSE) +
     ylim(c(0, max(node_xy_df$Y+10))) + xlim(c(0, max(node_xy_df$X+10))) +
     coord_fixed() + theme_void()
+  
+  
   
   g
 }
 
 #TODO: allow calculation for consensus edge for ALL vs. just a subset 
 # (i.e. consensus for certain indices, or patients with a certain label)
-consensus_edge <- function(diff_network_object, edge_weight="Weight", edge_color="Type"){
+consensus_edge <- function(diff_network_object, colors=NULL, nodetype="Node Type"){
  
   node1="N1"
   node2="N2"
@@ -299,25 +324,31 @@ consensus_edge <- function(diff_network_object, edge_weight="Weight", edge_color
   
   occurrences <- merge(occurrences, node_xy_df, by.x=node1, by.y="Node", all.x=TRUE, all.y=FALSE)
   occurrences <- merge(occurrences, node_xy_df, by.x=node2, by.y="Node", all.x=TRUE, all.y=FALSE)
+  occurrences$size = 1.5
   
   splits_occurrences <- split_loops(occurrences)
   
   edges_loops_occurrences <- splits_occurrences[[1]]
   edges_nonloops_occurrences <- splits_occurrences[[2]]
   
+  
+  if(is.null(colors)){
+    colors = sample(rainbow(2))
+  }
+  
   ggplot() + 
     geom_curve(
-      aes(x = X.x, y = Y.x, xend = X.y, yend = Y.y, size=abs(value), color=abs(value)),
+      aes(x = X.x, y = Y.x, xend = X.y, yend = Y.y, size=size, color=abs(value), alpha=0.8),
       data = edges_nonloops_occurrences
     ) +
     geom_curve(
-      aes(x = X.x, y = Y.x, xend = X.y, yend = Y.y, size=abs(value), color=abs(value)),  curvature = 50,angle = 270,
+      aes(x = X.x, y = Y.x, xend = X.y, yend = Y.y, size=size, color=abs(value)),alpha=0.8,  curvature = 50,angle = 270,
       data = edges_loops_occurrences
-    ) +
-    guides(size=FALSE) +
+    ) + scale_color_gradient(low=colors[1],high=colors[2]) + 
+    guides(size=FALSE) + gudes(alpha=FALSE) +
     guides(color=guide_colorbar(title="Number of Individuals with Edge")) +
     geom_circle(aes(x0 = X, y0 = Y, r = radius, fill = Node), data=node_xy_df) +geom_label_repel(aes(x=X,y=Y,label=Node),hjust=0, vjust=0, data=node_xy_df) +
-    guides(fill=guide_legend(title="Cell Type")) + 
+    guides(fill=guide_legend(title=nodetype)) + 
     ylim(c(0, max(node_xy_df$Y+10))) + xlim(c(0, max(node_xy_df$X+10))) +
     coord_fixed() + theme_void()
 } 
@@ -334,12 +365,15 @@ subset_network_plot <- function(network_plot, nodename){
   celltype_loc = q$data[[node_idx]][which(q$data[[node_idx]]$label == nodename),c("y","x")]
   
   for (i in 1:(length(q$data)-1)){
-    relevant_lines = c(which(q$data[[i]]$y == celltype_loc$y & q$data[[i]]$x == celltype_loc$x),
-                       which(q$data[[i]]$yend == celltype_loc$y & q$data[[i]]$xend == celltype_loc$x))
-    color_title = colnames(q$data[[i]])[grep("colour", colnames(q$data[[i]]))[1]]
-    q$data[[i]][setdiff(1:nrow(q$data[[i]]),relevant_lines),color_title] = "#D3D3D3"
-    q$data[[i]]$alpha = 1
-    q$data[[i]][setdiff(1:nrow(q$data[[i]]),relevant_lines),"alpha"] = 0.2
+    if ("yend" %in% colnames(q$data[[i]])){
+      relevant_lines = c(which(q$data[[i]]$y == celltype_loc$y & q$data[[i]]$x == celltype_loc$x),
+                         which(q$data[[i]]$yend == celltype_loc$y & q$data[[i]]$xend == celltype_loc$x))
+      color_title = colnames(q$data[[i]])[grep("colour", colnames(q$data[[i]]))[1]]
+      q$data[[i]][setdiff(1:nrow(q$data[[i]]),relevant_lines),color_title] = "#D3D3D3"
+      q$data[[i]]$alpha = 1
+      q$data[[i]][setdiff(1:nrow(q$data[[i]]),relevant_lines),"alpha"] = 0.2
+    }
+    
   }
   
 
@@ -451,7 +485,7 @@ make_piecharts <- function(diff_network_object, colors=c(), edge_feature=""){
   
 }
 
-make_piecharts_by_group <- function(diff_network_object, colors=c(), edge_feature="", group_label=""){
+make_piecharts_by_group <- function(diff_network_object, colors=c(), edge_feature="", group_label="", rows=0){
   
   node1 = "N1"
   node2 = "N2"
@@ -496,6 +530,9 @@ make_piecharts_by_group <- function(diff_network_object, colors=c(), edge_featur
 
   
   all_inx <- as.character(unique(do.call(rbind,tables)$Interaction))
+  if (rows != 0){
+    all_inx = all_inx[all_inx %in% rows]
+  }
   
   # create matrix to store distances
   #TODO: this will only happen if flag says we sort by distance vs. 
@@ -542,11 +579,15 @@ make_piecharts_by_group <- function(diff_network_object, colors=c(), edge_featur
     colors = rainbow(length(interaction_types)+1)
   }
   
+
   ggplot() + geom_scatterpie(aes(x=X, y=Y, r=Radius), data=merged_data, cols=c(edge_features, "Other"))  + coord_equal() +
     scale_x_discrete(limits = as.character(0:(length(groups)-1) + 0.5), breaks=as.character(0:(length(groups)-1) + 0.5), labels=groups, position="top") +
     scale_y_discrete(limits = as.character(0:(length(all_inx)-1) + 0.5), breaks=as.character(0:(length(all_inx)-1) + 0.5), labels=unique(merged_data[order(merged_data$Y, decreasing = FALSE), "Interaction"]))+ 
     theme_pubr() + theme(axis.text.x = element_text(angle = 45, hjust = 0, size=9), axis.text.y = element_text(size=9), axis.title.x = element_blank(), axis.title.y = element_blank())  + 
     scale_fill_manual(values=colors) + labs(fill = "Interaction Type")
+  
+  
+  
 }
 
 ############################ IGRAPH STUFF
